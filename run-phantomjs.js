1e91549c9d45e8c8a2327f577eb860629d83fdc7
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-01-26 15:30:18 +0100

    start webserver in phantomjs

--- run-phantomjs.js
+++ run-phantomjs.js
@@ -1,6 +1,6 @@
 console.log('starting phantomjs');
 var process = require('child_process')
-  .spawn(require('phantomjs').path, ["./dist.js", "7007"]);
+  .spawn(require('phantomjs').path || "phantomjs", ["./dist.js", "7007"]);
 process.stdout.on('data', function(s) { console.log(String(s).replace(/\n?$/, ""));});
 process.stderr.on('data', function(s) { console.error(String(s).replace(/\n?$/, ""));});
 process.on('exit', function(code) { process.exit(code); });
--- src/solsort/serverside.cljs
+++ src/solsort/serverside.cljs
@@ -8,3 +8,20 @@
   (:require [cljs.core.async :refer  [>! <! chan put! take! timeout close! pipe]]))
 
 (js/console.log "hello world")
+
+(-> (js/require "webserver")
+    (.create)
+    (.listen 
+      "127.0.0.1:2345"
+      (fn [req res]
+        ;; Handle request, phantom-api is:
+        ;; 
+        ;; - req: method, url, httpVersion, headers, post, postRaw
+        ;; - res: headers setHeader(name, value) header(name) statusCode, setCnoding(binary/utf-8), write(), writeHead(statusCode, headers), close(), closeGracefully()
+        (doto res
+          (.setHeader "Content-Type" "text/plain")
+          (aset "statusCode" 200)
+          (.write "hello")
+          (.close))
+        )))
+
