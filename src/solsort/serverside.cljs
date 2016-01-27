a053f26f37af2643a71f567ce5ca94abd7592158
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-01-27 12:06:23 +0100

    initial serverside rendering working, polish needed (if running latest phantomjs)

--- src/solsort/serverside.cljs
+++ src/solsort/serverside.cljs
@@ -1,14 +1,32 @@
 ;; # Serverside renderer
 ;;
-;; Plan: webservice for executing HTML5 webapps serverside
-;;
 ;; Status: system is now set up for executing clojurescript within phantomjs, and has nodejs runner for deploy with pm2
 ;;
 (ns solsort.serverside
   (:require [cljs.core.async :refer  [>! <! chan put! take! timeout close! pipe]]))
 
 (js/console.log "hello world")
+(def page-cache (atom '()))
 
+;; ## JavaScript function running within page in phantomjs
+;;
+;; This just sets a callback which calls back to the server
+(def phantom-function
+  "function () {
+  function done() {
+  onSolsortReady = undefined;
+  window.callPhantom ({
+  head: document.head.innerHTML,
+  body: document.body.innerHTML});
+  }
+  if (window.solsortLoading) {
+  onSolsortReady = done;
+  } 
+  setTimeout(done, 3000); 
+  }")
+
+
+;; ## Actual server
 (-> (js/require "webserver")
     (.create)
     (.listen 
@@ -18,10 +36,41 @@
         ;; 
         ;; - req: method, url, httpVersion, headers, post, postRaw
         ;; - res: headers setHeader(name, value) header(name) statusCode, setCnoding(binary/utf-8), write(), writeHead(statusCode, headers), close(), closeGracefully()
-        (doto res
-          (.setHeader "Content-Type" "text/plain")
-          (aset "statusCode" 200)
-          (.write "hello")
-          (.close))
-        )))
+        (let [url (-> (.-url req)
+                      (.slice 1)
+                      (.replace "/" "://")
+                      (.replace "?" "#"))
+              page (peek @page-cache)
+              page (if page
+                     (do (swap! page-cache pop) page)
+                     (.create (js/require "webpage")))
+              sent (atom false)
+              t0 (js/Date.now)
+              ]
+          (js/console.log url page)
+          (.open 
+            page url
+            (fn [status]
+              (aset 
+                page "onCallback"
+                (fn [data]
+                  (when-not @sent
+                    (js/console.log "blah")
+                    (reset! sent true)
+                    (doto res
+                      (.setHeader "Content-Type" "text/html")
+                      (aset "statusCode" 200)
+                      (.write 
+                        (str
+                          "<!DOCTYPE html><html><head>"
+                          (aget data "head")
+                          "</head><body>"
+                          (aget data "body")
+                          "</body></html>" ))
+
+                      (.close))
+                    (aset page "onCallback" nil)
+                    (swap! page-cache conj page))))
+              (.evaluateJavaScript page phantom-function)
+              ))))))
 
